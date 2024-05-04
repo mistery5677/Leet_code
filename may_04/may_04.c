5c1e1f559797173b35e128176466aa0255c38fc9
Miguel Costa <miguelcosta@Miguels-Air.lan>
2024-05-04 21:24:40 +0100

    updated

--- may_04/may_04.c
+++ may_04/may_04.c
@@ -1,57 +1,35 @@
 #include <stdio.h>
 
-void sort_people(int *people)
-{
-    int i = 0;
-    int j;
-    int temp = 0;
-
-    while(people[i])
-    {
-        j = i + 1;
-        while(people[j])
-        {
-            if(people[i] > people[j])
-            {
-                temp = people[j];
-                people[j] = people[i];
-                people[i] = temp; 
-            }
-            j++;
-        }
-        i++;
-    }
+int compare(const void * a, const void * b) {
+   return ( *(int*)b - *(int*)a );
 }
 
 int numRescueBoats(int* people, int peopleSize, int limit) 
 {
+    int right = peopleSize - 1;
+    int left = 0;
     int boats = 0;
-    int i = 0;
-    int weight = 0;
-    (void)peopleSize;
-    sort_people(people);
-    while(people[i])
+
+    qsort(people, peopleSize, sizeof(int), compare); 
+    while(left <= right)
     {
-        weight += people[i]; 
-        if(weight >= limit)
-        {
-            boats++;
-            weight = 0;
-        }
-        printf("weight: %d\n", weight);
-        i++;
+        if(people[left] + people[right] <= limit)
+            right--;
+        left++;
+        boats++;
     }
     return boats;    
 }
 
 
-int main() {
+/* int main() {
     int people[] = {3, 2, 2, 1};
     int peopleSize = sizeof(people) / sizeof(people[0]);
     int limit = 3;
 
+    printf("people size: %d\n", peopleSize);
     int boats = numRescueBoats(people, peopleSize, limit);
     printf("Number of boats needed: %d\n", boats);
 
     return 0;
-}
+} */
