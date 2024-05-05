9a1502958a962039dee360268f34d86f8143afe7
Miguel Costa <miguelcosta@Miguels-Air.lan>
2024-05-05 23:47:05 +0100

    updated

--- may_03/may_03.c
+++ may_03/may_03.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+
+
 int clear_zero(char *version, int start)
 {
     if(version[start] == '.')
         start++;
     while(version[start] == '0')
         start++;
-    if (version[start] == '.' || version[start] == NULL)
+    if (version[start] == '.' || version[start] == '\0')
         start--;
     return start;
 }
@@ -15,9 +17,9 @@ int clear_zero(char *version, int start)
 int value(char *version, int start)
 {
     int value = 0;
-    while(version[start] && version[start] != '.')
+    while(version[start] != '.' && version[start])
     {
-        value += (value * 10) + version[start];
+        value = (value * 10) + version[start];
         start++;  
     }
     return value;
@@ -33,14 +35,16 @@ int compareVersion(char* version1, char* version2)
     while(version1[i] && version2[j])
     {
         v1_value = value(version1, i);
+        printf("v1_value %d\n", v1_value);
         v2_value = value(version2, j);
+        printf("v2_value %d\n", v2_value);
         if(v1_value > v2_value)
             return 1;
         else if(v1_value < v2_value)
             return -1;
-        while(version1[i] != '.' && version1[i])
+        while(version1[i] != '.' && i <= strlen(version1))
             i++;
-        while(version2[j] != '.' && version2[j])
+        while(version2[j] != '.' && j <= strlen(version2))
             j++;
         i = clear_zero(version1, i);
         j = clear_zero(version2, j);
@@ -48,11 +52,10 @@ int compareVersion(char* version1, char* version2)
     return 0;
 }
 
-// proxima tentativa tentar juntar o numero num = num * version * 10
 int main()
 {
-    char *version1 = "1.2";
-    char *version2 = "1.10";
+    char *version1 = "1202.01";
+    char *version2 = "1202.001";
     int compare = compareVersion(version1, version2);
 
     printf("result= %d\n", compare);
